4c2118641cfc6761da39732cb9745dcff32bd2af
Ondřej Beneš <ondra.benes@gmail.com>
2011-12-13 15:27:26 +0100

    Initial Commit

---
+++ ColorGraph/OBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OBAppDelegate.h
+//  ColorGraph
+//
+//  Created by Ondra Beneš on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ColorGraph/OBAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  OBAppDelegate.m
+//  ColorGraph
+//
+//  Created by Ondra Beneš on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OBAppDelegate.h"
+
+@implementation OBAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
